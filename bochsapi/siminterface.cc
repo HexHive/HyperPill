94ee2264ecaccd9bc4098a24205d7d9aaee2dbad
Qiang Liu <cyruscyliu@gmail.com>
2025-03-25 12:49:59 +0100

    Revert "Disable SMAP for coverage"
    This reverts commit 0e7c5da377f24a2dd45e5ecbcc70cbedef579b2b.
    
    because cr4 will be overwritten by icp_init_regs().

--- bochsapi/siminterface.cc
+++ bochsapi/siminterface.cc
@@ -191,11 +191,7 @@ void icp_init_params() {
     bools["cpuid.fsgsbase"] = sim_new_param_bool("fsgsbase", true);
     bools["cpuid.pcid"] = sim_new_param_bool("pcid", true);
     bools["cpuid.smep"] = sim_new_param_bool("smep", true);
-    if (getenv("NOCOV")) {
-        bools["cpuid.smap"] = sim_new_param_bool("smap", true);
-    } else {
-        bools["cpuid.smap"] = sim_new_param_bool("smap", false);
-    }
+    bools["cpuid.smap"] = sim_new_param_bool("smap", true);
 
     bools["cpuid.mwait"] = sim_new_param_bool("mwait", false);
     bools["cpuid.movbe"] = sim_new_param_bool("movbe", false);
