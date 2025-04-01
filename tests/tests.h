1fdee9c1854a1e4e038db68e1929c278769925bc
Qiang Liu <cyruscyliu@gmail.com>
2025-04-01 16:05:09 +0200

    Add CVE-2022-0216

--- tests/tests.h
+++ tests/tests.h
@@ -1,15 +1,26 @@
 #ifndef HP_TESTS
 #include "fuzz.h"
 
-static void test_mem_write_up_to_8(uint64_t addr, size_t size, uint64_t data) {
-	BX_MEM(0)->writePhysicalPage(BX_CPU(id), addr, size, &data);
+static void test_mem_write(uint64_t addr, size_t size, void *data) {
+	BX_MEM(0)->writePhysicalPage(BX_CPU(id), addr, size, data);
+}
+
+static void test_mem_write_up_to_8(uint64_t addr, size_t size, uint64_t value) {
+	BX_MEM(0)->writePhysicalPage(BX_CPU(id), addr, size, &value);
 }
 
 // addr: guest physical address
-static void test_mmio_write(uint64_t addr, size_t size, uint64_t data) {
-	printf("[INJECT MMIO WRITE] addr: 0x%lx value: 0x%lx \n", addr, data);
-	if (!inject_write(addr, size, data))
+static void test_mmio_write(uint64_t addr, size_t size, uint64_t value) {
+	printf("[INJECT MMIO WRITE] addr: 0x%lx value: 0x%lx \n", addr, value);
+	if (!inject_write(addr, size, value))
 		printf("inject write error/n");
+	start_cpu();
+}
+
+static void test_out(uint16_t addr, uint16_t size, uint32_t value) {
+	printf("[INJECT OUT] addr: 0x%x value: 0x%x \n", addr, size);
+	if (!inject_out(addr, size, value))
+		printf("inject out error/n");
 	start_cpu();
 }
 
